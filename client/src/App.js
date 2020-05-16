6b11fd1b794658872a6277e76f5399bf87282a49
andvis <45904930+avistavkin@users.noreply.github.com>
2020-05-16 15:28:22 +0300

    commit202005161528

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Layout from './core/Layout';
 
 
 const App = () => {
   return(
-    <div>
-      <h1>Hello React</h1>
-    </div>
+    <Layout>
+      <h1>Hello LifeSet!</h1>
+    </Layout>
     );
 };
 
