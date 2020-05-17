ec09cf40bb6fbd873442a9d61cabda4b7c66e0a9
andvis <45904930+avistavkin@users.noreply.github.com>
2020-05-17 12:00:28 +0300

    commit202005171200

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,11 @@ import Layout from './core/Layout';
 
 const App = () => {
   return(
-    <Layout>
-      <h1>Hello LifeSet!</h1>
-    </Layout>
+		<Layout>
+			<div className="col-md-6 offset-md-3">
+					<h1 className="p-5 text-center">Welcome to xSet!</h1>
+			</div>
+		</Layout>
     );
 };
 
