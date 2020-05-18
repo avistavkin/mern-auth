f00daba8266075addfbb992c1e58f325d19924a9
andvis <45904930+avistavkin@users.noreply.github.com>
2020-05-18 08:35:33 +0300

    commit202005180835

--- client/src/auth/Signup.js
+++ client/src/auth/Signup.js
@@ -1,25 +1,48 @@
 import React, {useState} from 'react';
-import {Link, Redirect} from 'react-router-dom';
+//import {Link, Redirect} from 'react-router-dom';
 import Layout from '../core/Layout';
 import axious from 'axios';
 import {ToastContainer, toast} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
 const Signup = () => {
-	const [values, setValue] = useState({
+	const [values, setValues] = useState({
 		name: '',
 		email: '',
 		password: '',
 		buttonText: 'Submit' // текст кнопки - будем менять динамически
 	});
 
-	const {name, email, password, buttonText} = values
+	const {name, email, password, buttonText} = values;
 
-	const handleChange = (name) => (event) => {
-
+	const handleChange = (name) => (event) => { //обработчики событий
+		console.log(event.target.value); //для новичков вывод логов в консоль
+		setValues({ ...values, [name]: event.target.value});
 	};
 
-	const clickSubmit = event => {
+	const clickSubmit = event => { //обработчик нажатия на кнопку
+		event.preventDefault()
+		setValues({ ...values, buttonText: 'Submitting'});
+		axious({
+			method: 'POST',
+			url: `${process.env.REACT_APP_API}/signup`,   //берём url из файла .env*/
+			data: {name, email, password}
+		})
+
+		.then(response => {
+			console.log('SIGNUP SUCCESS', response);
+			//как только мы получили success - нам обязательно надо почистить state!
+			setValues({...values, name: '', email: '', password: '', buttonText: 'Submitted'});
+			toast.success(response.data.message);
+		})
+		.catch(error => {
+			console.log('SIGNUP ERROR', error.response.data); 
+			//error.response.data - данные ошибки будут переданы из бэкенда.
+			setValues({...values, buttonText: 'Submit'});
+			toast.error(error.response.data.error);
+
+		});
+
 
 
 	};
@@ -39,16 +62,19 @@ const Signup = () => {
 				<input onChange={handleChange('password')} value = {password} type="password" className="form-control"/>
 			</div>
 			<div>
-				<button className="btn btn-primary" onClick={clickSubmit}>{buttonText}</button>
+				<button className="btn btn-primary" onClick={clickSubmit}>{buttonText} </button>
+
 			</div>
 		</form>
 
-		);
+	);
 
+//{JSON.stringify({name, email, password})};  очень полезный дебагер - выводит прямо на экран*/
 	return (
 		<Layout>
 			<div className="col-md-6 offset-md-3">
 				<ToastContainer />
+				{JSON.stringify({name, email, password})}; 
 				<h1 className="p-5 text-center">Signup to xSet!</h1>
 				{signupForm()}
 			</div>
