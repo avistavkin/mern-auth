1d76fbe902bd3bf42e10a21ce039dd2941c3652c
andvis <45904930+avistavkin@users.noreply.github.com>
2020-05-20 21:32:17 +0300

    commit202005202132

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import Layout from './core/Layout';
 
 
+
 const App = () => {
   return(
 		<Layout>
 			<div className="col-md-6 offset-md-3">
 					<h1 className="p-5 text-center">Welcome to xSet!</h1>
+					<img  className="left" src="https://avatars2.githubusercontent.com/u/17990669?s=400&u=ff71ac184a10f2dde4a994c736cd805177e5aae6&v=4" alt="xSet" width="300" height="300" fluid = "true"></img>
 			</div>
+
+		
 		</Layout>
     );
 };
+
+
 
 
 export default App;
