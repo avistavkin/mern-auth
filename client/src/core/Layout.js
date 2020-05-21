f2e589c4a4c7d44d485bfa70e3ed205abdb25795
andvis <45904930+avistavkin@users.noreply.github.com>
2020-05-21 21:55:00 +0300

    commit202005212155

--- client/src/core/Layout.js
+++ client/src/core/Layout.js
@@ -1,9 +1,10 @@
 import React, {Fragment} from 'react';
 import {Link, withRouter} from 'react-router-dom'
+import {isAuth, signout} from '../auth/helpers'
 
-const Layout = ({children, match}) => {
+const Layout = ({children, match, history}) => {
 	const isActive = path => {
-		if(match.path == path) {
+		if(match.path === path) {
 			return {color: '#000'}
 		} else {
 			return {color: '#fff'}
@@ -18,16 +19,55 @@ const Layout = ({children, match}) => {
 					Home
 				</Link>
 			</li>
-			<li className="nav-item">
-				<Link to="/signin" className="nav-link" style={isActive('/signin')}>
-					Signin
-				</Link>
-			</li>
-			<li className="nav-item">
-				<Link to="/signup" className="nav-link" style={isActive('/signup')}>
-					Signup
-				</Link>
-			</li>
+
+			{!isAuth() && (
+				<Fragment>
+
+					<li className="nav-item">
+						<Link to="/signin" className="nav-link" style={isActive('/signin')}>
+							Signin
+						</Link>
+					</li>
+					<li className="nav-item">
+						<Link to="/signup" className="nav-link" style={isActive('/signup')}>
+							Signup
+						</Link>
+					</li>
+
+				</Fragment>
+
+			)}
+
+			{isAuth() && (
+				<li className="nav-item">
+					<span 
+						className="nav-link" 
+						style={{cursor: 'pointer', color: '#fff'}} 
+						>
+						{isAuth().name}'s profile
+		
+						</span>
+
+				</li>
+
+			)}
+
+			{isAuth() && (
+				<li className="nav-item">
+					<span 
+						className="nav-link"
+						style={{cursor: 'pointer', color: '#fff'}} 
+						onClick={() => {
+						signout(() => {
+							history.push('/')
+						})
+					}}>
+						Signout
+					</span>
+
+				</li>
+
+			)}
 		</ul>
 
 		)
